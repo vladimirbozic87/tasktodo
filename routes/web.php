9396c4c643d87298408a27efbb3e02ea8beb52dd
vladimirbozic87 <vladimirbozic87@gmail.com>
2017-04-23 21:34:12 +0200

    git commit tasktodo

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [
+	       'uses' => '\App\Http\Controllers\HomeController@index',
+           'as' => 'home'
+]);
+
+
+/**
+*Authentication
+*/
+
+Route::get('/signin', [
+           'uses' => '\App\Http\Controllers\AuthController@getSignin',
+           'as' => 'auth.signin',
+           'middleware' => ['guest'],
+]);
+
+Route::post('/signin', [
+            'uses' => '\App\Http\Controllers\AuthController@postSignin',
+            'middleware' => ['guest'],
+]);
+
+Route::get('/signup', [
+           'uses' => '\App\Http\Controllers\AuthController@getSignup',
+           'as' => 'auth.signup',
+           'middleware' => ['guest'],
+]);
+
+Route::post('/signup', [
+            'uses' => '\App\Http\Controllers\AuthController@postSignup',
+            'middleware' => ['guest'],
+]);
+
+Route::get('/signout', [
+           'uses' => '\App\Http\Controllers\AuthController@getSignout',
+           'as' => 'auth.signout',
+]);
+
+
+/**
+*user Profile
+*/
+
+Route::get('/user/{username}', [
+    'uses' => '\App\Http\Controllers\ProfileController@getProfile',
+    'as' => 'profile.index', 
+]);
